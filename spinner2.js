b9b2d6965f04fdd874a1d60081b946fb35244eb0
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-06 15:28:17 -0500

    added file spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,15 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const spinnerChars = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let delay = 100;
+
+for (const char of spinnerChars) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+  }, delay);
+  delay += 200;
+}
+
+setTimeout(() => {
+  console.log();
+}, delay);
